097b021571227d85fb0a511eb7acd515dd32c2db
Iven Schlenther <iven.s@lvckyworld.net>
2024-04-14 16:18:56 +0200

    [TASK] fix animation duration and update package-lock.json

--- src/app/site-notice/page.tsx
+++ src/app/site-notice/page.tsx
@@ -16,10 +16,10 @@ export default function NotFound() {
 
     return (
         <motion.div
-            initial={{opacity: 0, scale: 0.6}}
+            initial={{opacity: 0, scale: 0.95}}
             animate={{opacity: 1, scale: 1}}
             exit={{opacity: 0, scale: 0.95}}
-            transition={{ease: "easeOut", duration: 0.5}}
+            transition={{ease: "easeOut", duration: 0.15}}
             className="lw-content-container"
         >
 
